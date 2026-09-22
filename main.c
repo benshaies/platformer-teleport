3139c68b36d58f1f2a382de4a7850ccbc0141fcd
StillBenja <benshaies17@gmail.com>
2026-09-22 19:09:53 -0400

    update

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
 #include <raylib.h>
+
+int main(){
+  
+  InitWindow(1280, 720, "platformer");
+  SetTargetFPS(60);
+
+  while(!WindowShouldClose()){
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    DrawRectangle(500, 500, 50, 50, BLACK);
+
+    EndDrawing();
+  }
+}
